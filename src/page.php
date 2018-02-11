21f141493a6c52855690f1978b1c310bb126d6ef
Hillie Kootstra <hillie@MacBook-Pro-van-Hillie.local>
2018-02-11 14:07:06 +0100

    add default page styling

--- src/page.php
+++ src/page.php
@@ -1,45 +1,66 @@
 <?php get_header(); ?>
+	<div class="default-page">
+		<div class="title-wrapper">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-offset-1 col-sm-12 col-xs-12">
+						<h1><?php the_title(); ?></h1>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- post thumbnail -->
+		<!-- <?php if ( has_post_thumbnail()) : // Check if Thumbnail exists ?>
+			<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+				<?php the_post_thumbnail(); // Fullsize image for the single post ?>
+			</a>
+		<?php endif; ?> -->
+		<!-- /post thumbnail -->
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-offset-1 col-sm-12 col-xs-12">
+					<main role="main" aria-label="Content">
+						<!-- section -->
+						<section>
 
-	<main role="main" aria-label="Content">
-		<!-- section -->
-		<section>
+							
 
-			<h1><?php the_title(); ?></h1>
+						<?php if ( have_posts()) : while ( have_posts() ) : the_post(); ?>
 
-		<?php if ( have_posts()) : while ( have_posts() ) : the_post(); ?>
+							<!-- article -->
+							<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-			<!-- article -->
-			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+								<div class="wysiwyg-content">
+								<?php the_content(); ?>
+								</div>
 
-				<?php the_content(); ?>
+								<br class="clear">
 
-				<?php comments_template( '', true ); // Remove if you don't want comments. ?>
+								<?php edit_post_link(); ?>
 
-				<br class="clear">
+							</article>
+							<!-- /article -->
 
-				<?php edit_post_link(); ?>
+						<?php endwhile; ?>
 
-			</article>
-			<!-- /article -->
+						<?php else : ?>
 
-		<?php endwhile; ?>
+							<!-- article -->
+							<article>
 
-		<?php else : ?>
+								<h2><?php esc_html_e( 'Sorry, nothing to display.', 'html5blank' ); ?></h2>
 
-			<!-- article -->
-			<article>
+							</article>
+							<!-- /article -->
 
-				<h2><?php esc_html_e( 'Sorry, nothing to display.', 'html5blank' ); ?></h2>
+						<?php endif; ?>
 
-			</article>
-			<!-- /article -->
-
-		<?php endif; ?>
-
-		</section>
-		<!-- /section -->
-	</main>
-
-<?php get_sidebar(); ?>
+						</section>
+						<!-- /section -->
+					</main>
+				</div>
+			</div>
+		</div>
+	</div>
 
 <?php get_footer(); ?>
