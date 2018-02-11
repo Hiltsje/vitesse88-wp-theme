3eaf7f77ee0ceb38af2b90a6a8c03654f01ff673
Hillie Kootstra <hillie@MacBook-Pro-van-Hillie.local>
2018-02-11 14:57:28 +0100

    add default single page styling

--- src/single.php
+++ src/single.php
@@ -1,71 +1,79 @@
 <?php get_header(); ?>
+<div class="default-single">
+	<div class="title-wrapper">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-offset-1 col-sm-12 col-xs-12">
+					<a href="/"> <span>Nieuws</span></a>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-offset-1 col-sm-12 col-xs-12">
+				<main role="main" aria-label="Content">
+				<!-- section -->
+				<section>
 
-	<main role="main" aria-label="Content">
-	<!-- section -->
-	<section>
+				<?php if ( have_posts() ) : while (have_posts() ) : the_post(); ?>
 
-	<?php if ( have_posts() ) : while (have_posts() ) : the_post(); ?>
+					<!-- article -->
+					<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-		<!-- article -->
-		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+						<!-- post thumbnail -->
+						<?php if ( has_post_thumbnail() ) : // Check if Thumbnail exists. ?>
+							<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+								<?php the_post_thumbnail(); // Fullsize image for the single post. ?>
+							</a>
+						<?php endif; ?>
+						<!-- /post thumbnail -->
 
-			<!-- post thumbnail -->
-			<?php if ( has_post_thumbnail() ) : // Check if Thumbnail exists. ?>
-				<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
-					<?php the_post_thumbnail(); // Fullsize image for the single post. ?>
-				</a>
-			<?php endif; ?>
-			<!-- /post thumbnail -->
+						<!-- post title -->
+						<h1>
+							<?php the_title(); ?>
+						</h1>
+						<!-- /post title -->
 
-			<!-- post title -->
-			<h1>
-				<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
-			</h1>
-			<!-- /post title -->
+						<!-- post details -->
+						
+						<span class="date">
+							<hr>
+							<time datetime="<?php the_time( 'Y-m-d' ); ?>">
+								<img src="<?php echo get_template_directory_uri(); ?>/img/icons/icon-calander.svg">	
+								plaatsdatum: <?php the_date(); ?>
+							</time>
+							<hr>
+						</span>
+						
+						<!-- /post details -->
 
-			<!-- post details -->
-			<span class="date">
-				<time datetime="<?php the_time( 'Y-m-d' ); ?> <?php the_time( 'H:i' ); ?>">
-					<?php the_date(); ?> <?php the_time(); ?>
-				</time>
-			</span>
-			<span class="author"><?php esc_html_e( 'Published by', 'html5blank' ); ?> <?php the_author_posts_link(); ?></span>
-			<span class="comments"><?php if ( comments_open( get_the_ID() ) ) comments_popup_link( __( 'Leave your thoughts', 'html5blank' ), __( '1 Comment', 'html5blank' ), __( '% Comments', 'html5blank' ) ); ?></span>
-			<!-- /post details -->
+						<?php the_content(); // Dynamic Content. ?>
 
-			<?php the_content(); // Dynamic Content. ?>
+						<?php edit_post_link(); // Always handy to have Edit Post Links available. ?>
 
-			<?php the_tags( __( 'Tags: ', 'html5blank' ), ', ', '<br>' ); // Separated by commas with a line break at the end. ?>
+					</article>
+					<!-- /article -->
 
-			<p><?php esc_html_e( 'Categorised in: ', 'html5blank' ); the_category( ', ' ); // Separated by commas. ?></p>
+				<?php endwhile; ?>
 
-			<p><?php esc_html_e( 'This post was written by ', 'html5blank' ); the_author(); ?></p>
+				<?php else : ?>
 
-			<?php edit_post_link(); // Always handy to have Edit Post Links available. ?>
+					<!-- article -->
+					<article>
 
-			<?php comments_template(); ?>
+						<h1><?php esc_html_e( 'Sorry, nothing to display.', 'html5blank' ); ?></h1>
 
-		</article>
-		<!-- /article -->
+					</article>
+					<!-- /article -->
 
-	<?php endwhile; ?>
+				<?php endif; ?>
 
-	<?php else : ?>
-
-		<!-- article -->
-		<article>
-
-			<h1><?php esc_html_e( 'Sorry, nothing to display.', 'html5blank' ); ?></h1>
-
-		</article>
-		<!-- /article -->
-
-	<?php endif; ?>
-
-	</section>
-	<!-- /section -->
-	</main>
-
-<?php get_sidebar(); ?>
-
+				</section>
+				<!-- /section -->
+				</main>
+			</div>
+		</div>
+	</div>
+</div>
 <?php get_footer(); ?>
