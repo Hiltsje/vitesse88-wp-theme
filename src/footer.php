3bb4129d2ea6bc27244bec8a25a1d4137ed46a15
Hillie Kootstra <hillie@MacBook-Pro-van-Hillie.local>
2018-03-01 20:11:51 +0100

    add analytics to footer

--- src/footer.php
+++ src/footer.php
@@ -48,7 +48,7 @@ Mail/bel ons of meld je aan via contact.</p>
 		(f[s].q=f[s].q||[]).push(arguments)},f[s].l=1*new Date();h=i.createElement(r),
 		l=i.getElementsByTagName(r)[0];h.async=1;h.src=e;l.parentNode.insertBefore(h,l)
 		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-		ga('create', 'UA-XXXXXXXX-XX', 'yourdomain.com');
+		ga('create', 'UA-10720398-4', 'vitesse88.nl');
 		ga('send', 'pageview');
 		</script>
 
